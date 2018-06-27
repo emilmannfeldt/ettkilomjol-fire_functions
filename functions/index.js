5680b61c44aa7273749f6e9ec9c691495741e125
emilmannfeldt <emil.mannfeldt@gmail.com>
2018-06-27 23:04:03 +0200

    initial commit

---
+++ functions/index.js
@@ -0,0 +1,21 @@
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+
+admin.initializeApp(functions.config().firebase);
+
+exports.verifyInvitation = functions.https.onRequest((request, response) => {
+    response.header('Access-Control-Allow-Origin', '*');
+
+    //googla på firebase order ovan
+    return admin.database().ref('config/invitation_code').once('value', (snapshot) => {
+        if (snapshot.val() === request.query.code) {
+            response.status(200).send("OK");
+        } else {
+            console.log("wrong input:" + request.query.code);
+            response.status(400).send('not OK');
+        }
+    }).catch(error => {
+        console.log(erro);
+        response.status(500).send(error);
+    })
+});
